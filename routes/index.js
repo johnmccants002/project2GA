4bce64667115a7d7a3da3e32ec9969b50365402a
johnmccants002 <johnmccants002@gmail.com>
2022-05-05 01:10:04 -0700

    OAuth working

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 import { Router } from 'express'
+import passport from 'passport'
+
+
+
 
 const router = Router()
 
@@ -6,6 +10,24 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
 })
 
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 export {
   router
 }
